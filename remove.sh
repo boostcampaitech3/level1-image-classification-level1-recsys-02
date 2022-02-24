77ae30fd51cd63cc943483e657d5bfe7a9ee563f
pilkyuchoi <eat7777@naver.com>
2022-02-24 21:29:22 +0900

    update

---
+++ remove.sh
@@ -0,0 +1,7 @@
+rm -rf /opt/ml/mask/model/exp_adam
+rm -rf /opt/ml/age/model/exp_adam
+rm -rf /opt/ml/gender/model/exp_adam
+rm /opt/ml/mask/output/exp_adam.csv
+rm /opt/ml/age/output/exp_adam.csv
+rm /opt/ml/gender/output/exp_adam.csv
+rm /opt/ml/submission/exp_adam.csv
