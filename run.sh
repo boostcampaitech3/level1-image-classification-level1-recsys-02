415f2647b322247a6a26c2521acd71a50bea7eb9
pilkyuchoi <eat7777@naver.com>
2022-02-25 11:40:58 +0900

    update

--- run.sh
+++ run.sh
@@ -1,11 +1,7 @@
-cd mask
-python train.py --model Resnet18 --epochs 10 --name exp_adam --optimizer 'Adam'
-python inference.py --model Resnet18 --name exp_adam
-cd ../gender
-python train.py --model Resnet18 --epochs 10 --name exp_adam --optimizer 'Adam'
-python inference.py --model Resnet18 --name exp_adam
-cd ../age
-python train.py --model Resnet18 --epochs 10 --name exp_adam --optimizer 'Adam'
-python inference.py --model Resnet18 --name exp_adam
-cd
-python submission.py --name exp_adam
+python mask/train.py --model Resnet18 --epochs 15 --name exp2
+python mask/inference.py --model Resnet18 --name exp2
+python gender/train.py --model Resnet18 --epochs 15 --name exp2
+python gender/inference.py --model Resnet18 --name exp2
+python age/train.py --model Resnet18 --epochs 15 --name exp2
+python age/inference.py --model Resnet18 --name exp2
+python ../submission.py --name exp2 --mask_name exp2 --gender_name exp2 --age_name exp2
