79516915d0b85251902d379c328e4155314951dc
pilkyuchoi <eat7777@naver.com>
2022-03-02 06:49:27 +0000

    update

--- remove.sh
+++ remove.sh
@@ -1,12 +1,12 @@
 # remove model directory
-rm -rf /opt/ml/model/exp_b3
-rm -rf /opt/ml/model/exp_b3
-rm -rf /opt/ml/model/exp_b3
+rm -rf model/exp_b3
+rm -rf model/exp_b3
+rm -rf model/exp_b3
 
 # remove output.csv
-rm /opt/ml/output/exp_b3.csv
-rm /opt/ml/output/exp_b3.csv
-rm /opt/ml/output/exp_b3.csv
+rm output/exp_b3.csv
+rm output/exp_b3.csv
+rm output/exp_b3.csv
 
 # remove submission.csv
-rm /opt/ml/submission/exp_b3.csv
+rm submission/exp_b3.csv
