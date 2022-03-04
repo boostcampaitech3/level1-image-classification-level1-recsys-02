6548a48c474f4af3632640a0d65ad1624fc83fa2
pilkyuchoi <eat7777@naver.com>
2022-03-04 02:12:44 +0000

    update

--- inference.py
+++ inference.py
@@ -1,6 +1,9 @@
 import argparse
 import os
 from importlib import import_module
+from glob import glob
+from pathlib import Path
+import re
 
 import pandas as pd
 import numpy as np
@@ -35,18 +38,21 @@ def inference(data_dir, model_dir, output_dir, args):
 
     num_classes = task_classes[args.task]
     oof_pred = None
+    info_path = os.path.join(data_dir, 'info.csv')
+    info = pd.read_csv(info_path)
     n_splits = args.n_splits
     print("Calculating inference results..")
+
+    info_path = os.path.join(data_dir, 'info.csv')
+    info = pd.read_csv(info_path)
+    img_root = os.path.join(data_dir, 'crop_images')
+    img_paths = [os.path.join(img_root, img_id) for img_id in info.ImageID]
+
     #OOF Ensemble
     for i in range(n_splits):
         model = load_model(model_dir, num_classes, device, i).to(device)
         model.eval()
 
-        img_root = os.path.join(data_dir, 'crop_images')
-        info_path = os.path.join(data_dir, 'info.csv')
-        info = pd.read_csv(info_path)
-
-        img_paths = [os.path.join(img_root, img_id) for img_id in info.ImageID]
         dataset = TestDataset(img_paths, args.resize)
         loader = torch.utils.data.DataLoader(
             dataset,
@@ -58,18 +64,83 @@ def inference(data_dir, model_dir, output_dir, args):
         )
 
         preds = []
+        # soft Voting
         with torch.no_grad():
             for idx, images in enumerate(loader):
                 images = images.to(device)
                 # TTA
                 pred = model(images) / 2
                 pred += model(torch.flip(images, dims=(-1,))) / 2 # Horizontal_flip
-                pred = pred.argmax(dim=-1)
                 preds.extend(pred.cpu().numpy())
+            fold_pred = np.array(preds)
 
-        info['ans'] = preds
-        info.to_csv(os.path.join(output_dir, f'{args.name}_{i}_fold.csv'), index=False)
+        if oof_pred is None:
+            oof_pred = fold_pred / n_splits
+        else:
+            oof_pred += fold_pred / n_splits
+            
+
+    info['ans'] = torch.tensor(oof_pred).argmax(dim=-1)
+    info.to_csv(os.path.join(output_dir, f'{args.name}.csv'), index=False)
+            
     print(f'Inference Done!')
+
+def hardvoting(data_dir,save_file_name):
+    all_files = glob(f'{data_dir}/*.csv') 
+    li = []
+
+    # file path에 따라 파일 로드
+    for filename in all_files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        # 결과 저장
+        li.append(df['ans'])
+
+    # 결과 종합 concatnate
+    frame = pd.concat(li, axis=1, ignore_index=True)
+    df['ans'] = frame.mode(axis=1)[0]
+    df['ans'] = df['ans'].astype(int)
+
+    df.to_csv(f'{data_dir}/{save_file_name}.csv')
+
+#3개 조합을 0~17숫자로
+def encode_multi_class(mask_label, gender_label, age_label):
+    return mask_label * 6 + gender_label * 3 + age_label
+
+#파일 덮어쓰기 방지
+def increment_path(path, exist_ok=False):
+    """ Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
+
+    Args:
+        path (str or pathlib.Path): f"{model_dir}/{args.name}".
+        exist_ok (bool): whether increment path (increment if False).
+    """
+    path = Path(path)
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob(f"{path}*")
+        matches = [re.search(rf"%s(\d+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]
+        n = max(i) + 1 if i else 2
+        return f"{path}{n}"
+
+def combine():
+        # 3개 모델 예측 결과 18 클래스로 변환
+        # 파일 불러오기
+        mask = pd.read_csv(f'./output/{args.mask}.csv')
+        gender = pd.read_csv(f'./output/{args.gender}.csv')
+        age = pd.read_csv(f'./output/{args.age}.csv')
+
+        #3개 파일 합치기
+        df_ = pd.merge(mask,gender, on='ImageID')
+        df = pd.merge(df_,age, on='ImageID')
+
+        for i in range(len(df)):
+            df.loc[i, 'ans'] = encode_multi_class(df.iloc[i]['ans_x'],df.iloc[i]['ans_y'],df.iloc[i]['ans'])
+
+        #필요한 칼럼만 남기고 저장
+        df = df[['ImageID','ans']]
+        df.to_csv(os.path.join('./preds', increment_path(args.model)) + '.csv', index=False)
 
 
 if __name__ == '__main__':
@@ -88,6 +159,15 @@ if __name__ == '__main__':
     parser.add_argument('--name', type=str, default='exp')
     parser.add_argument('--output_dir', type=str, default=os.environ.get('SM_OUTPUT_DATA_DIR', './output'))
 
+    # Combine
+    parser.add_argument('--combine', default=False)
+    parser.add_argument('--mask', type=str, default='mask')
+    parser.add_argument('--gender', type=str, default='gender')
+    parser.add_argument('--age', type=str, default='age')
+
+    # Model Ensembel
+    parser.add_argument('--model_ensemble', default=False)
+
     args = parser.parse_args()
 
     data_dir = args.data_dir
@@ -96,4 +176,10 @@ if __name__ == '__main__':
 
     os.makedirs(output_dir, exist_ok=True)
 
-    inference(data_dir, model_dir, output_dir, args)
+    if args.model_ensemble:
+        hardvoting(f'output/preds', 'preds')
+
+    elif args.combine:
+        combine()
+    else:
+        inference(data_dir, model_dir, output_dir, args)
